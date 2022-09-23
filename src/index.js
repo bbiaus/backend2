4893c998012a1ccb5641fbaf7d2e2d96720b47d0
bbiaus <biaus.boris@gmail.com>
2022-09-22 22:48:12 -0300

    Proyecto clase4 completo

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import Container from "./containers/Container.js";
+
+const ProductContainer = new Container("productos");
+
+
+/* ProductContainer.getAll()
+  .then((data) => console.log({ data }))
+  .catch((error) => console.log({ error })); */
+
+/* ProductContainer.save({
+  title: "Producto 1",
+  price: 500,
+  thumbnail: "httpsejemplo",
+})
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error)); */
+
+/* ProductContainer.getById(2)
+  .then((pepe) => console.log({ pepe }))
+  .catch((error) => console.log({ error })); */
+
+/* ProductContainer.deleteById()
+  .then((data) => console.log({ data }))
+  .catch((error) => console.log(error)); */
+
+/* ProductContainer.deleteAll(); */
